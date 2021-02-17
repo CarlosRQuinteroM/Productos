20ec44b7cef1d83586208dccfa50db899708089a
CarlosRQuinteroM <carlosquintero92.cq@gmail.com>
2021-02-17 14:26:19 +0100

    Agregando la funcion de resetar el dormulario y agregando el boton  de borrar elemento en la lista de productos .

--- main.js
+++ main.js
@@ -14,18 +14,27 @@ class UI {
         element.innerHTML = `
         <div class="card text-center mb-4">
            <div class="card-body">
-              <strong>Producto</strong>:${producto.name} 
-              <strong>Precio del Producto</strong>:${producto.price}
-              <strong>Año del Producto</strong>:${producto.year}
+              <strong>Producto</strong>: ${producto.name}
+              <strong>Precio del Producto</strong>: ${producto.price}
+              <strong>Año del Producto</strong>: ${producto.year}
+              <a href="#" onClick="" name="borrar" class="btn btn-danger">Borrar</a>
  
            </div>
 
         </div>
-        `
+        `;
+        productList.appendChild(element);
+        
     }
+
+    resetForm(){
+       document.getElementById("productos-form").reset();
+    }
+
     deleteProduct(){
 
     }
+
     showMassage(){
 
     }
@@ -41,7 +50,14 @@ document.getElementById('productos-form')
 
     const producto = new Producto(name, price , year);
 
-
+     const ui = new UI();
+     ui.addproduct(producto);
+     ui.resetForm(); 
+    
 
     e.preventDefault();
 });
+
+document.getElementById('Product-list').addEventListener('click' , function()){
+    arlert('capturado')
+}
